b7ea1796644542b8d2a0bf701a4497625ad1e777
Bhashini Yasam <81588022+bhashiniy@users.noreply.github.com>
2023-02-21 11:44:03 -0500

    proper image copying

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -23,26 +23,19 @@ function updateSlidePosition() {
     slide.classList.remove('visible');
     slide.classList.add('hidden');
   });
-
+  console.log(slidePosition); 
   slides[slidePosition].classList.add('visible'); 
 }
   
-  // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
-  // loop through all the slides in your slideArray
-  // and remove the 'visible' class from each classList
-  // then add a class 'hidden' to all of them
-
-  // outside your .forEach,
-  // add a 'visible' class to the slide at the current slidePosition in slides
-
 
 function moveToNextSlide() {
-  /*
-    add an if statement here that checks
-    if you're already at the max number of slides
-    and if so, sets your slidePosition to the first index of an array
-    if not, set the slidePosition to the current position plus one
-  */
+
+  if(slidePosition===totalSlides - 1){
+    slidePosition = 0; }
+      else{
+        slidePosition += 1; 
+      }
+
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
