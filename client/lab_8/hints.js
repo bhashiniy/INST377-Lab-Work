7978dcd5e0b1e1893154d73d70d1960ba1702fd7
Bhashini Yasam <81588022+bhashiniy@users.noreply.github.com>
2023-05-01 19:50:50 -0400

    lab8

--- client/lab_8/hints.js
+++ client/lab_8/hints.js
@@ -5,3 +5,32 @@ map.eachLayer((layer) => {
     layer.remove();
   }
 });
+
+function initMap() {
+  const map = L.map("map").setView([38.9897, -76.9378], 13);
+  
+  return map;
+}
+
+const map = initMap();
+
+function markerPlace(restaurants) {
+
+  markersLayer.clearLayers();
+
+
+  restaurants.forEach((restaurant) => {
+    const latLng = L.latLng(restaurant["coordinates"][1], restaurant["coordinates"][0]);
+    const marker = L.marker(latLng).addTo(markersLayer);
+    marker.bindPopup(`<b>${restaurant["name"]}</b><br>${restaurant["category"]}`).openPopup();
+  });
+
+
+  const firstRestaurantLatLng = L.latLng(restaurants[0]["coordinates"][1], restaurants[0]["coordinates"][0]);
+  map.panTo(firstRestaurantLatLng);
+}
+
+async function fetchData() {
+
+  const restaurants = await response.json();
+}
